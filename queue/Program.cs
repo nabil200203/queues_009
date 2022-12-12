6e31ebac4072a746bf5f8625ce6b066007a54036
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-12-12 17:04:18 +0700

    add void insert

--- queue/Program.cs
+++ queue/Program.cs
@@ -26,8 +26,29 @@ namespace queue
             /*This statement check for the overflow condition. */
             if ((FRONT == 0 && REAR == max -1) ||(FRONT == REAR +1))
             {
-
+                Console.WriteLine("\nQueue overflow\n");
+                return;
             }
+
+            /* This following statement check the queue is empty. if the queue is empty
+             * then the value of the REAR and  FRONT variables is set to 0 */
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* if REAR is at the position of the array. then the value of
+                 * RAER is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* if REAR is not at the lasrt position, then its value is incremented by one */
+                    REAR = REAR + 1;
+            }
+            /* Once the position of RAER is deterwined, the element is added at its proper place */
+            queue_array[REAR] = element;
         }
         static void Main(string[] args)
         {
