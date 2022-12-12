1e6a63fb6a14698d735d0d7bee8999399f2c49e9
TC1009 <m.nabil.ft21@mail.umy.ac.id>
2022-12-12 16:45:30 +0700

    add public program

--- queue/Program.cs
+++ queue/Program.cs
@@ -11,6 +11,15 @@ namespace queue
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
+
+        public Program()
+        {
+            /*initializing the values of the variables REAR and FRONT to -1 to indicate that
+             * the name is initiall;y empty,*/
+
+            FRONT = -1;
+            REAR = -1;
+        }
         static void Main(string[] args)
         {
         }
